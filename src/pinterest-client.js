84d1b74448db189cccdf43c68e24f307ab04de01
kiennt <trungkien2288@gmail.com>
2015-03-10 17:13:30 +0700

    Change tab

--- src/pinterest-client.js
+++ src/pinterest-client.js
@@ -9,23 +9,23 @@ const DELETE = "delete";
 var request = require('request');
 
 export class PinterestClient {
-	constructor(authKey) {
-		this.authKey = authKey;
-	}
+  constructor(authKey) {
+    this.authKey = authKey;
+  }
 
-	getURL(methodName, path, params) {
-		let url = `${DOMAIN}/${path}`;
+  getURL(methodName, path, params) {
+    let url = `${DOMAIN}/${path}`;
 
-		if (params) {
-			let stringOfParams = "";
-			for (var key in params) {
-				stringOfParams += `${key}=${params[key]}&`;
-			};
-			url += `?${stringOfParams}`;
-		}
+    if (params) {
+      let stringOfParams = "";
+      for (var key in params) {
+        stringOfParams += `${key}=${params[key]}&`;
+      };
+      url += `?${stringOfParams}`;
+    }
 
-		return url;
-	}
+    return url;
+  }
 
   get(path, params={}) {
     return this.request(GET, path, params)
@@ -43,51 +43,51 @@ export class PinterestClient {
     return this.request(DELETE, path, params, data)
   }
 
-	request(methodName, path, params={}, data={}) {
-		let url = this.getURL(methodName, path, params);
-		console.log(url);
+  request(methodName, path, params={}, data={}) {
+    let url = this.getURL(methodName, path, params);
+    console.log(url);
 
-		var func;
-		switch(methodName) {
-			case PUT:
-				func = request.put;
-				break;
-			case POST:
-				func = request.post;
-				break;
-			case DELETE:
-				func = request.delete;
-				break;
-			default:
-				break;
-		}
+    var func;
+    switch(methodName) {
+      case PUT:
+        func = request.put;
+        break;
+      case POST:
+        func = request.post;
+        break;
+      case DELETE:
+        func = request.delete;
+        break;
+      default:
+        break;
+    }
 
-		return func({
-			url: url,
-			formData: {
-				access_token: this.authKey
-			}
-		}, function (error, response, body) {
-			if (!error) {
-				switch(response.statusCode) {
-					case 200:
-						console.log(body);
-						break
-					case 401:
-						console.log(body);
-						break
-					default:
-				}
-			} else {
-				throw error;
-			}
-		});
-	}
+    return func({
+      url: url,
+      formData: {
+        access_token: this.authKey
+      }
+    }, function (error, response, body) {
+      if (!error) {
+        switch(response.statusCode) {
+          case 200:
+            console.log(body);
+            break
+          case 401:
+            console.log(body);
+            break
+          default:
+        }
+      } else {
+        throw error;
+      }
+    });
+  }
 
-	likeAPin(pinId) {
-		let params = {
-			"access_token" : this.authKey
-		};
-		this.request(PUT, `pins/${pinId}/like/`, null, params);
-	}
+  likeAPin(pinId) {
+    let params = {
+      "access_token" : this.authKey
+    };
+    this.request(PUT, `pins/${pinId}/like/`, null, params);
+  }
 }
