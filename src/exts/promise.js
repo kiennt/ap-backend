82f1f9762f4f83b8a9972bf04f103df060cae69e
Nova <novadev94@gmail.com>
2015-03-12 15:13:16 +0700

    Add Promise.delay function

---
+++ src/exts/promise.js
@@ -0,0 +1,9 @@
+'use strict';
+
+Promise.delay = function delay(timeout) {
+  return function delayedPromise(...args) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(...args), timeout);
+    });
+  };
+};
--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import { Bot } from './bot';
+require('./exts/promise');
 
 /*eslint-disable*/
 const ACCESS_TOKEN = 'MTQzMTU5NDo1MzcyNjU1Njc5NTQ5NDcxMjM6OTIyMzM3MjAzNjg1NDc3NTgwNzoxfDE0MjU4OTA3NjE6MC0tMDI2NTg5N2U2NzhjODUyYTlhMmY3MzhjZjVmMGY0MDE=';
 /*eslint-disable*/
 const BOT_TYPE = 'pinterest';
 
-let bot = new Bot(ACCESS_TOKEN, BOT_TYPE);
-bot.run();
+// let bot = new Bot(ACCESS_TOKEN, BOT_TYPE);
+// bot.run();
+
+
+// Demo code for Promise.delay
+console.log('start');
+Promise.resolve(1)
+  .then(Promise.delay(1000))
+  .then((x) => console.log(x))
+  .then(() => Promise.resolve(2))
+  .then(Promise.delay(2000))
+  .then((x) => console.log(x))
+  .then(() => console.log('end'));
