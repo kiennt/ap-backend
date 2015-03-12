3bf2ee5d6a7205157b55ba53088d1b12b1a13633
kiennt <trungkien2288@gmail.com>
2015-03-12 15:23:30 +0700

    Using jasmine instead of jest

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,32 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
 var eslint = require('gulp-eslint');
+var jasmine = require('gulp-jasmine');
 
-gulp.task('default', ['lint'], function () {
-  return gulp.src('src/**/*.js')
+function es6to5(input, output) {
+  return gulp.src(input)
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(output));
+}
+
+gulp.task('dist', ['lint'], function () {
+  return es6to5('src/**/*.js', 'dist');
+});
+
+gulp.task('spec-dist', function () {
+  return es6to5('spec/**/*.js', 'spec-dist');
 });
 
 gulp.task('lint', function () {
-  return gulp.src(['src/**/*.js'])
+  return gulp.src(['src/**/*.js', 'spec/**/*.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
+});
+
+gulp.task('test', ['lint', 'dist', 'spec-dist'], function () {
+  return gulp.src(['spec-dist/**/*.js'])
+    .pipe(jasmine());
 });
