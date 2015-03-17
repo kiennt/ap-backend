ed1d82acf464db01b5b02a458a734ff5a90bf75a
Nova <novadev94@gmail.com>
2015-03-18 00:05:16 +0700

    Fix: Tabs & spaces

--- src/config/http-headers.js
+++ src/config/http-headers.js
@@ -1,47 +1,47 @@
 const HTTP_HEADERS = [
-	{'X-Pinterest-Device': 'GT-I9300',
-	'X-Pinterest-AppState': 'background',
-	'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.1.2)'},
-	{'X-Pinterest-Device': 'SM-G900V',
-	'X-Pinterest-AppState': 'background',
-	'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.4.4)'},
-	{'X-Pinterest-Device': 'SM-N915A',
-	'X-Pinterest-AppState': 'background',
-	'User-Agent': 'Pinterest for Android/4.3.0 (c1lgt; 4.4.4)'},
-	{'X-Pinterest-Device': 'Z998',
-	'X-Pinterest-AppState': 'background',
-	'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.1.2)'},
-	{'X-Pinterest-Device': 'XT1254',
-	'X-Pinterest-AppState': 'background',
-	'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.4.4)'},
-	{'X-Pinterest-Device': 'SGH-I527',
-	'X-Pinterest-AppState': 'background',
-	'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.2.2)'},
-	{'X-Pinterest-Device': 'SM-N900V',
-	'X-Pinterest-AppState': 'background',
-	'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.4.4)'},
-	{'X-Pinterest-Device': 'XT1080',
-	'X-Pinterest-AppState': 'background',
-	'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.4.4)'},
-	{'X-Pinterest-Device': 'SM-G900P',
-	'X-Pinterest-AppState': 'background',
-	'User-Agent': 'Pinterest for Android/4.3.0 (c1lgt; 4.4.4)'},
-	{'X-Pinterest-Device': 'SM-N900T',
-	'X-Pinterest-AppState': 'background',
-	'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.3)'},
-	{'X-Pinterest-Device': 'SGH-M919',
-	'X-Pinterest-AppState': 'background',
-	'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.4.4)'},
-	{'X-Pinterest-Device': 'SM-G900V',
-	'X-Pinterest-AppState': 'background',
-	'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.4.4)'}
+  {'X-Pinterest-Device': 'GT-I9300',
+  'X-Pinterest-AppState': 'background',
+  'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.1.2)'},
+  {'X-Pinterest-Device': 'SM-G900V',
+  'X-Pinterest-AppState': 'background',
+  'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.4.4)'},
+  {'X-Pinterest-Device': 'SM-N915A',
+  'X-Pinterest-AppState': 'background',
+  'User-Agent': 'Pinterest for Android/4.3.0 (c1lgt; 4.4.4)'},
+  {'X-Pinterest-Device': 'Z998',
+  'X-Pinterest-AppState': 'background',
+  'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.1.2)'},
+  {'X-Pinterest-Device': 'XT1254',
+  'X-Pinterest-AppState': 'background',
+  'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.4.4)'},
+  {'X-Pinterest-Device': 'SGH-I527',
+  'X-Pinterest-AppState': 'background',
+  'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.2.2)'},
+  {'X-Pinterest-Device': 'SM-N900V',
+  'X-Pinterest-AppState': 'background',
+  'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.4.4)'},
+  {'X-Pinterest-Device': 'XT1080',
+  'X-Pinterest-AppState': 'background',
+  'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.4.4)'},
+  {'X-Pinterest-Device': 'SM-G900P',
+  'X-Pinterest-AppState': 'background',
+  'User-Agent': 'Pinterest for Android/4.3.0 (c1lgt; 4.4.4)'},
+  {'X-Pinterest-Device': 'SM-N900T',
+  'X-Pinterest-AppState': 'background',
+  'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.3)'},
+  {'X-Pinterest-Device': 'SGH-M919',
+  'X-Pinterest-AppState': 'background',
+  'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.4.4)'},
+  {'X-Pinterest-Device': 'SM-G900V',
+  'X-Pinterest-AppState': 'background',
+  'User-Agent': 'Pinterest for Android/4.3.1 (c1lgt; 4.4.4)'}
 ];
 
 function getRandomHeaders() {
-	let index = Math.floor(Math.random() * 1000 % HTTP_HEADERS.length);
-	return HTTP_HEADERS[index];
+  let index = Math.floor(Math.random() * 1000 % HTTP_HEADERS.length);
+  return HTTP_HEADERS[index];
 }
 
 export default {
-	randomHeaders: getRandomHeaders
+  randomHeaders: getRandomHeaders
 };
