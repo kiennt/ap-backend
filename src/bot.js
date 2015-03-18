044d4301ff368dc28c0653f487e4dd6afc0e6da4
Daniel Nguyen <dxungngh@gmail.com>
2015-03-18 23:42:48 +0700

    fix bot

--- src/bot.js
+++ src/bot.js
@@ -9,21 +9,22 @@ export class Bot {
   }
 
   run() {
-    // this.client.likeAPin('83879611786469438')
-    //   .then((body) => console.log(body))
-    //   .catch((err) => console.log(err));
+    this.client.likeAPin('83879611786469438')
+      .then((status) => console.log(status))
+      .catch((err) => console.log(err));
 
-    // this.client.followUser('10414780296729982')
-    //   .then((body) => console.log(body))
-    //   .catch((err) => console.log(err));
+    this.client.followUser('10414780296729982')
+      .then((status) => console.log(status))
+      .catch((err) => console.log(err));
 
-    // this.client.commentAPin('83879611786469438', 'cool')
-    //   .then((body) => console.log(body))
-    //   .catch((err) => console.log(err));
+    this.client.commentAPin('83879611786469438', 'cool')
+      .then((status) => console.log(status))
+      .catch((err) => console.log(err));
 
-    // this.client.getInfoOfMe()
-    //   .then((body) => console.log(body))
-    //   .catch((err) => console.log(err));
+    this.client.getInfoOfMe()
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+
     this.client.search('dungnh', 1, 'user')
       .then((body) => console.log(body))
       .catch((body) => console.log(body));
