f55be7db36515f9b691ef637d611f90bcc41401e
Daniel Nguyen <dxungngh@gmail.com>
2015-03-20 00:21:55 +0700

    remove done

--- spec/lib/authentication.spec.js
+++ spec/lib/authentication.spec.js
@@ -2,7 +2,7 @@ import Authentication from '../../dist/lib/authentication';
 import HttpHeaders from '../../dist/config/http-headers';
 
 describe('generateSignature', () => {
-  it('should return true when generating signature', (done) => {
+  it('should return true when generating signature', () => {
     let data = {
       'password': 'k',
       'timestamp': '1395914520',
@@ -11,6 +11,5 @@ describe('generateSignature', () => {
     let url = 'https://api.pinterest.com/v3/login/';
     let signature = Authentication.generateSignature('POST', url, data);
     expect(signature).toBe('6ee35c775b5f92668530d9cc2b91d9380c4bf01f1b17ccfa73ecfd2867b7b562');
-    done();
   });
 });
