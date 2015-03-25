81e3c3e5ccd3dee46eda85bad994be6ef5366927
Nova <novadev94@gmail.com>
2015-03-25 15:26:47 +0700

    Refactor

--- src/exts/lodash.js
+++ src/exts/lodash.js
@@ -1,22 +1,15 @@
 import _ from 'lodash';
 
 _.randomBoolean = function randomBoolean() {
-  if (_.random(1) === 0) {
-    return false;
-  } else {
-    return true;
-  }
+  return Boolean(_.random(1));
 };
 
 _.randomSample = function randomSample(collection, min, max) {
-  let begin = 1;
-  let end = collection.length;
-  if (min !== undefined) {
-    begin = Math.floor(min * collection.length / 100);
-  }
-  if (max !== undefined) {
-    end = Math.floor(max * collection.length / 100);
-  }
+  min = min || 0;
+  max = max || 100;
+
+  let begin = Math.floor(min * collection.length / 100);
+  let end = Math.floor(max * collection.length / 100);
 
   return _.sample(collection, _.random(begin, end));
 };
