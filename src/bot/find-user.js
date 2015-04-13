6872b80a1fe3e6e607706183dee91169a45a479d
Nova <novadev94@gmail.com>
2015-04-13 17:22:31 +0700

    Move code to pinterest/client

--- src/bot/find-user.js
+++ src/bot/find-user.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import Promise from 'bluebird';
 
-import PinterestApi from '../pinterest/api';
+import PinterestClient from '../pinterest/client';
 import Authentication from '../lib/authentication';
 import HttpHeaders from '../config/http-headers';
 
@@ -12,11 +12,6 @@ let targetQuery = 'Nova Trương';
 let targetUsername = 'novadev94';
 let targetId = '???';
 
-let maxSearchPage = _.random(5, 7);
-
-let autocompleteNotFound = 0x1234;
-let searchNotFound = 0x4321;
-
 let predicate = (e) => (e.username === targetUsername || e.id === targetId);
 
 
@@ -24,66 +19,18 @@ export class Bot {
   constructor(accessToken, httpHeaders, type) {
     switch (type) {
       case 'pinterest':
-        this.api = new PinterestApi(accessToken, httpHeaders);
+        this.client = new PinterestClient(accessToken, httpHeaders);
     }
-  }
-
-  performAutocomplete(query, sliceIndex=0) {
-    if (sliceIndex > query.length) {
-      throw autocompleteNotFound;
-    } else {
-      let newQuery = query.slice(0, sliceIndex);
-      console.log(`* Autocomplete: '${newQuery}'`);
-      return this.api.getAutoCompleteText(newQuery)
-        .then((data) => _(data).filter('type', 'user').value())
-        .then((data) => _(data).filter(predicate).value()[0])
-        .then((result) => {
-          if (result) {
-            return result;
-          } else {
-            console.log('=> Not found! Continue with next character!');
-            return this.performAutocomplete(query, sliceIndex + 1);
-          }
-        });
-    }
-  }
-
-  performSearch(query, currentPage, bookmark) {
-    currentPage = currentPage || 1;
-    console.log(`* Perform an user search with: '${query}' | ${bookmark}`);
-    return this.api.search(query, 25, 'user', bookmark)
-      .then((body) => {
-        let result = _(body.data).filter(predicate).value()[0];
-        if (result) {
-          return result;
-        } else {
-          let nextBookmark = body.bookmark;
-          if (nextBookmark === bookmark || currentPage >= maxSearchPage) {
-            throw searchNotFound;
-          } else {
-            return Promise.delay(this, _.random(2000, 4000))
-              .then(() => this.performSearch(
-                query, currentPage + 1, nextBookmark));
-          }
-        }
-      });
   }
 
   run() {
-    this.performAutocomplete(targetQuery)
-      .catch((error) => {
-        if (error === autocompleteNotFound) {
-          return Promise.delay(this, _.random(100, 2000))
-            .then(() => this.performSearch(targetQuery));
-        } else {
-          throw error;
-        }
-      })
+    this.client.findAnUser(targetQuery, predicate)
       .then((user) => {
         // We found the user by using appropriate behaviors
         // There's no risk about it
         console.log(user);
       })
-      .catch(() => console.error('Can not found with any ways'));
+      .catch((e) => e.name === 'SearchNotFound',
+        (e) => console.error('Can not found with any ways'));
   }
 }
--- src/pinterest/client.js
+++ src/pinterest/client.js
@@ -1,7 +1,12 @@
 import _ from 'lodash';
 import Promise from 'bluebird';
 
+import {customError} from '../lib/errors'
 import PinterestApi from './api';
+
+
+let AutocompleteNotFound = customError('AutocompleteNotFound');
+let SearchNotFound = customError('SearchNotFound');
 
 
 export default class PinterestClient {
@@ -9,5 +14,58 @@ export default class PinterestClient {
     this.api = new PinterestApi(accessToken, httpHeaders);
   }
 
-  // Methods go here
+  hello() {
+    console.log(10000);
+  }
+
+  findAnUser(fullName, predicate) {
+    let maxPage = _.random(5, 7);
+    return this._autocompleteUser(fullName, predicate)
+      .catch(AutocompleteNotFound, (error) => {
+        return Promise.delay(this, _.random(100, 2000))
+          .then(() => this._searchUser(fullName, predicate, maxPage));
+      });
+  }
+
+  _autocompleteUser(query, predicate, sliceIndex) {
+    sliceIndex = sliceIndex || 0;
+    if (sliceIndex > query.length) {
+      throw new AutocompleteNotFound(query);
+    } else {
+      let newQuery = query.slice(0, sliceIndex);
+      console.log(`* Autocomplete: '${newQuery}'`);
+      return this.api.getAutoCompleteText(newQuery)
+        .then((data) => _(data).filter('type', 'user').value())
+        .then((data) => _(data).filter(predicate).value()[0])
+        .then((result) => {
+          if (result) {
+            return result;
+          } else {
+            console.log('=> Not found! Continue with next character!');
+            return this._autocompleteUser(query, predicate, sliceIndex + 1);
+          }
+        });
+    }
+  }
+
+  _searchUser(query, predicate, maxPage, currentPage, bookmark) {
+    currentPage = currentPage || 1;
+    console.log(`* Perform an user search with: '${query}' | ${bookmark}`);
+    return this.api.search(query, 25, 'user', bookmark)
+      .then((body) => {
+        let result = _(body.data).filter(predicate).value()[0];
+        if (result) {
+          return result;
+        } else {
+          let nextBookmark = body.bookmark;
+          if (nextBookmark === bookmark || currentPage >= maxPage) {
+            throw new SearchNotFound([query, maxPage]);
+          } else {
+            return Promise.delay(this, _.random(2000, 4000))
+              .then(() => this._searchUser(
+                query, predicate, maxPage, currentPage + 1, nextBookmark));
+          }
+        }
+      });
+  }
 }
