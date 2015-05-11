635ed11bccc974ca6f557f6129dd04d982f0dc57
Daniel Nguyen <dxungngh@gmail.com>
2015-05-11 17:00:19 +0700

    tab -> space

--- autopin-database/api/controllers/AccountController.js
+++ autopin-database/api/controllers/AccountController.js
@@ -9,11 +9,11 @@ module.exports = {
   delete: function(req, res) {
     var account = req.options.account;
     account.destroy()
-    	.then(function() {
-    		return res.send();
-    	})
-    	.catch(function(err) {
-    		return res.error(400, 'Something is wrong. Please try again', err);
-    	});
+      .then(function() {
+        return res.send();
+      })
+      .catch(function(err) {
+        return res.error(400, 'Something is wrong. Please try again', err);
+      });
   },
 }
