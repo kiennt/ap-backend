8fc75092c32391958fd8c5f3fa1a293b45aa02c2
Daniel Nguyen <dxungngh@gmail.com>
2015-05-11 17:02:02 +0700

    change to parseInt

--- autopin-database/api/policies/isValidAccount.js
+++ autopin-database/api/policies/isValidAccount.js
@@ -1,6 +1,6 @@
 module.exports = function isValidAccount (req, res, next) {
   var user = req.options.user;
-  var accountId = _.parseInt(req.body.id);
+  var accountId = parseInt(req.body.id);
   if (accountId) {
     var accountFound = _(user.accounts).find(function(account) {
       return (account.id === accountId);
