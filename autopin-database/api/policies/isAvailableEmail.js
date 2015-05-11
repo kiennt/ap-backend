5b65dbe5497332032dfbc4e3954f3caa2fc4415c
Daniel Nguyen <dxungngh@gmail.com>
2015-05-11 20:18:51 +0700

    fix comments

--- autopin-database/api/policies/isAvailableEmail.js
+++ autopin-database/api/policies/isAvailableEmail.js
@@ -3,15 +3,15 @@ module.exports = function isAvailableEmail (req, res, next) {
   var query = {
     email: email
   };
-  User.count(query)
-    .then(function(numberOfUser) {
-      if (numberOfUser > 0) {
+  User.findOne(query)
+    .then(function(userFound) {
+      if (userFound) {
         return res.error(400, 'Email already exist');
       } else {
         next();
       }
     })
-    .catch(function(err)  {
+    .catch(function(err) {
       return res.error(500, 'Something wrong. Please try again', err);
     });
 };
