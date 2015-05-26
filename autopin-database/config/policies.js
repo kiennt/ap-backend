e361942ada9ababa492afb8b1db4c8345d9600fb
Daniel Nguyen <dxungngh@gmail.com>
2015-05-26 15:17:38 +0700

    singup

--- autopin-database/config/policies.js
+++ autopin-database/config/policies.js
@@ -36,6 +36,7 @@ module.exports.policies = {
   ***************************************************************************/
   UserController: {
     signin: 'hasEmailAndPassword',
+    signup: ['hasEmailAndPassword', 'isEmailAvailable'],
     signupWithPinterestAccount: ['hasEmailAndPassword', 'isEmailAvailable'],
     getUser: 'isAuthenticated',
     updateUser: 'isAuthenticated',
